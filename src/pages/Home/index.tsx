14e6009135b9de414191837082f387fb806ce5ff
tresagua0123 <gentlekai10@gmail.com>
2021-01-30 12:08:37 +0900

    introduced react material-ui.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GlobalHeader from "src/components/templates/GlobalHeader";
+
+const Home: React.FC = () => {
+    return (
+    <>
+    <GlobalHeader />
+    <div>Home だよ</div>
+    </>)
+}
+
+export default Home;
